0715b20e8e1c35dc7b6f0ad992bffe5b880a7d01
leehaesong3966 <leehaesong3966@naver.com>
2023-08-18 22:12:49 +0900

    feat: 잠시 수정

--- src/main/java/com/help/hyozason_backend/controller/helprequestsse/HelpRequestSSEController.java
+++ src/main/java/com/help/hyozason_backend/controller/helprequestsse/HelpRequestSSEController.java
@@ -26,7 +26,7 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 //@RequiredArgsConstructor //Lombok으로 스프링에서 DI(의존성 주입)의 방법 중에 생성자 주입을 임의의 코드없이 자동으로 설정해주는 어노테이션
 @RestController //@RestController 어노테이션은 사용된 클래스의 모든 메서드에 자동으로 JSON 변환을 적용
 @RequestMapping("/help/helprequest")
-@CrossOrigin(origins = "http://223.130.139.203:3000", allowCredentials = "true")
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 @Slf4j
 public class HelpRequestSSEController {
     private final SseEmitters sseEmitters;
